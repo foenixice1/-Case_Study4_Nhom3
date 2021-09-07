81f1caecf0b30ecac588e0d8521367b7af92128b
Nguyễn Hoài Nam <83760339+foenixice1@users.noreply.github.com>
2021-09-07 14:23:02 +0700

    Case

---
+++ src/main/java/com/example/case_study4_nhom3/CaseStudy4Nhom3Application.java
@@ -0,0 +1,13 @@
+package com.example.case_study4_nhom3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudy4Nhom3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudy4Nhom3Application.class, args);
+	}
+
+}
